ecb22089be94e526f2b1db1fda5282b1ba86ef27
pranavshp <udayakumarp2@vcu.edu>
2020-01-26 22:01:34 -0500

    Updates

---
+++ precache-manifest.02a4de5b7abfa537e5a9dde5f639d1cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87a1487bcac6bb7810d2974f897671c",
+    "url": "/state-management/index.html"
+  },
+  {
+    "revision": "1d5932112dccf8172a14",
+    "url": "/state-management/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "77aea7265789121f6cfb",
+    "url": "/state-management/static/js/2.f591e593.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/state-management/static/js/2.f591e593.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d5932112dccf8172a14",
+    "url": "/state-management/static/js/main.f9161ce9.chunk.js"
+  },
+  {
+    "revision": "4e2577b8e450a8ecb421",
+    "url": "/state-management/static/js/runtime-main.b37d24dc.js"
+  }
+]);
